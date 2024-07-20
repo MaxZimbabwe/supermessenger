098a5d6773fadba7e927ca935422d782187a47b6
MaxZimbabwe <viinicius-rn@hotmail.com>
2024-07-20 02:25:06 -0300

    fix:feat: sera ajustado a inicializacao da api

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
-from app import create_app
+import sys
 
-app = create_app()
+# Ativar o ambiente virtual
+activate_this = '/supermessenger-main/.venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Adicionar o caminho do projeto ao sys.path
+sys.path.insert(0, '/supermessenger-main')
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
